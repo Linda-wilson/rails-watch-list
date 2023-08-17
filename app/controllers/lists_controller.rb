2cd895d37a0f60c91a9906a923a51ecf0614607c
linda <lindawilson.may21@gmail.com>
2023-08-17 20:41:27 +0200

    watchList

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,28 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def show
+    raise
+    @list = List.find(params[:id])
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list, notice: 'List successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+end
+
+private
+def list_params
+  params.require(:list).permit(:name)
+end
